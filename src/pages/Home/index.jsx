63afed753db5d045a9094ceb99b7917317808ced
Eniko GUTI <enikoguti@macbook-pro-de-eniko.home>
2022-03-24 15:42:11 +0100

    @mediaquery pages Home & Accomodation added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,28 +5,19 @@ import { Link } from 'react-router-dom'
 import Thumb from '../../components/thumb'
 import '../Home/home.css'
 
-
-
 function Home() {
   //const {accomodationData, error} = GetAccomodations(`http://localhost:3000/accomodation`)
   console.log(accomodation.accomodation)
 
-  
   return (
     <div className="home-container">
       <Banner />
-      <div className="accomodation-container">
-      
-      {accomodation.accomodation.map((house)=> (
-          
-           
+      <div className="accomodation-container">        
+        {accomodation.accomodation.map((house)=> (
           <Link key={house.id} to={`accomodation/${house.id}`} >
             <Thumb image={house.cover} title={house.title}/>
-
           </Link>
-
         ))}
-
       </div>
     </div>
   )
